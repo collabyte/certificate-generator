e12c01b52224cbfd49015c5eea2a4864d6c6e171
scifisatan <brawlershere@gmail.com>
2024-05-21 15:28:13 +0545

    chore: setup shdcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
+import { Button } from "@/components/ui/button";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+      <Button>Click me</Button>
     </>
   );
 }
